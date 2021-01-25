91f4442eae27e2834e8bff10ef943506c6bbaaba
denis <denis.scarlat@gmail.com>
2021-01-25 13:56:46 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
-import AddTodo from "./Todo/AddTodo";
+import Loader from "./loader";
+import Modal from "./Modal/Modal";
+
+const AddTodo = React.lazy(() => import("./Todo/AddTodo"));
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    { id: 1, completed: false, title: "De cumparat paine" },
-    { id: 2, completed: false, title: "De cumparat unt" },
-    { id: 3, completed: false, title: "De cumparat lapte" },
-  ]);
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000);
+      });
+  }, []); // masivul gol e lista de dependente de care va avea nevoie callback functia
 
   function toggleTodo(id) {
     setTodos(
@@ -41,10 +52,17 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>React tutorial</h1>
-        <AddTodo onCreate={addTodo} />
+        <Modal />
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+
+        {loading && <Loader />}
+        {/* daca loading=true - aratam Loader */}
+
         {todos.length ? (
           <TodoList todos={todos} onToggle={toggleTodo} />
-        ) : (
+        ) : loading ? null : (
           <p>No todos!</p>
         )}
       </div>
